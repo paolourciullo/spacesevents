731005493f93611cdfe42d0fd8d98608f619aa77
Paolo U <paolourciullo@gmail.com>
2021-06-01 22:20:20 -0400

    add alias and jsconfig

--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,8 @@
 import { FaExclamationTriangle } from 'react-icons/fa';
 import Link from 'next/link';
-import Layout from '../components/Layout';
+import Layout from '@/components/Layout';
 
-import styles from '../styles/404.module.css';
+import styles from '@/styles/404.module.css';
 
 export default function NotFoundPage() {
   return (
