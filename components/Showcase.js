da2a3fcfab98ec28f3c39c8ddf00b15d01b6320a
Paolo U <paolourciullo@gmail.com>
2021-06-01 22:39:38 -0400

    add showcase

---
+++ components/Showcase.js
@@ -0,0 +1,10 @@
+import styles from '@/styles/Showcase.module.css';
+
+export default function Showcase() {
+  return (
+    <div className={styles.showcase}>
+      <h1>Welcome to the Party, yo</h1>
+      <h2>Find the hottest DJ Events, yo</h2>
+    </div>
+  );
+}
