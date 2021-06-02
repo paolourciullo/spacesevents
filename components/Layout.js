f4ecad7b864ff7f670d0aaa1795fcd15bbfcf0b8
Paolo U <paolourciullo@gmail.com>
2021-06-01 22:00:08 -0400

    create layout, header, footer

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,6 @@
 import Head from 'next/head';
+import Header from './Header';
+import Footer from './Footer';
 import styles from '../styles/Layout.module.css';
 
 export default function Layout({ title, keywords, description, children }) {
@@ -9,8 +11,9 @@ export default function Layout({ title, keywords, description, children }) {
         <meta name="description" content={description} />
         <meta name="keywords" content={keywords} />
       </Head>
-
+      <Header />
       <div className={styles.container}>{children}</div>
+      <Footer />
     </div>
   );
 }
