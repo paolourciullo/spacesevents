754944652a9e040bf4a3a1607a37f4977bf03911
Paolo U <paolourciullo@gmail.com>
2021-06-05 12:43:54 -0400

    add search fxn

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Search from './Search';
 import styles from '@/styles/Header.module.css';
 
 export default function Header() {
@@ -9,7 +10,7 @@ export default function Header() {
           <a>DJ Events</a>
         </Link>
       </div>
-
+      <Search />
       <nav>
         <ul>
           <li>
