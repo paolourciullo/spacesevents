7e62a9241cfefed8a2f3c10d4337b724e98c667e
Paolo U <paolourciullo@gmail.com>
2021-07-13 18:24:10 -0400

    add persist user

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -45,7 +45,14 @@ export const AuthProvider = ({ children }) => {
 
   // logout user
   const logout = async () => {
-    console.log('Logout')
+    const res = await fetch(`${NEXT_URL}/api/logout`, {
+      method: 'POST',
+    })
+
+    if (res.ok) {
+      setUser(null)
+      router.push('/')
+    }
   }
 
   // check if user is logged in
