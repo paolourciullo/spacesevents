de3d079ef8a546b8f6d252e402c7fc0fcf000190
Paolo U <paolourciullo@gmail.com>
2021-07-13 18:16:02 -0400

    create cookie

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -1,12 +1,16 @@
 import { createContext, useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
-import { API_URL } from '@/config/index'
+import { NEXT_URL } from '@/config/index'
 
 const AuthContext = createContext()
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null)
   const [error, setError] = useState(null)
+
+  const router = useRouter()
+
+  useEffect(() => checkUserLoggedIn(), [])
 
   // Register user
   const register = async (user) => {
@@ -15,7 +19,28 @@ export const AuthProvider = ({ children }) => {
 
   // Login user
   const login = async ({ email: identifier, password }) => {
-    console.log({ identifier, password })
+    const res = await fetch(`${NEXT_URL}/api/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        identifier,
+        password,
+      }),
+    })
+
+    const data = await res.json()
+
+    console.log(data)
+
+    if (res.ok) {
+      setUser(data.user)
+      router.push('/account/dashboard')
+    } else {
+      setError(data.message)
+      setError(null)
+    }
   }
 
   // logout user
@@ -25,7 +50,14 @@ export const AuthProvider = ({ children }) => {
 
   // check if user is logged in
   const checkUserLoggedIn = async (user) => {
-    console.log('Check')
+    const res = await fetch(`${NEXT_URL}/api/user`)
+    const data = await res.json()
+
+    if (res.ok) {
+      setUser(data.user)
+    } else {
+      setUser(null)
+    }
   }
 
   return (
