5f924be3f8ff9788fd1400205fff03fdbcc2147b
Paolo U <paolourciullo@gmail.com>
2021-07-19 09:51:23 -0400

    update info in about

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,12 @@
-import Head from 'next/head';
-import { useRouter } from 'next/router';
-import Header from './Header';
-import Footer from './Footer';
-import Showcase from './Showcase';
-import styles from '@/styles/Layout.module.css';
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import Header from './Header'
+import Footer from './Footer'
+import Showcase from './Showcase'
+import styles from '@/styles/Layout.module.css'
 
 export default function Layout({ title, keywords, description, children }) {
-  const router = useRouter();
+  const router = useRouter()
 
   return (
     <div>
@@ -20,11 +20,11 @@ export default function Layout({ title, keywords, description, children }) {
       <div className={styles.container}>{children}</div>
       <Footer />
     </div>
-  );
+  )
 }
 
 Layout.defaultProps = {
-  title: 'DJ Events | Find the hottest parties yo',
-  description: 'Find the latest DJ and other musical events, yo',
-  keywords: 'music, dj, edm, events, yo ',
-};
+  title: 'Spaces Events | Twitter Spaces',
+  description: 'Find interesting twitter spaces',
+  keywords: 'twitter, spaces, tech, blog',
+}
--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,20 @@
-import Layout from '@/components/Layout';
+import Layout from '@/components/Layout'
 
 export default function AboutPage() {
   return (
-    <Layout title="About Dj Events yo">
-      <h1>About page is online and ready to go yo!</h1>
-      <p>This is an app to find the latest DJ and other musical events cuz</p>
+    <Layout title="About SpacesEvents 👇">
+      <h1>by Paolo w/ ❤</h1>
+      <p>
+        Twitter spaces is a way for twitter users to create an audio pipeline to
+        their followers and also give their followers a chance to chime in. What
+        you get is instant feedback from your community, which can be a highly
+        valuable resource.{' '}
+      </p>
+      <p>
+        Feel free to use this app! If you have any issues when using the app
+        please reach out to me via twitter @paolo__init__ .{' '}
+      </p>
       <p>version: 1.0.0</p>
     </Layout>
-  );
+  )
 }
